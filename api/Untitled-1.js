9b6be95065dacb783c170267025784b96b3b0673
qwertauda <qwertauda@gmail.com>
2025-12-03 19:31:21 +0300

    Fix webhook body parsing

--- api/Untitled-1.js
+++ api/Untitled-1.js
@@ -4,7 +4,8 @@ const { exec } = require('child_process');
 const path = require('path');
 const app = express();
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(express.static(path.join(__dirname, '..', 'public'))); // للخدمات الثابتة
 
 // Authentication
