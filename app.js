fa347a04fc717be499e2c2870938022a550ee0ee
qwertauda <qwertauda@gmail.com>
2025-12-06 19:49:14 +0300

    تحديث: تغيير جميع النصوص من 7 أيام إلى 3 أيام

--- app.js
+++ app.js
@@ -4905,7 +4905,7 @@ async function renderExpiringSoonList() {
             descEl.textContent = 'قائمة مخصصة للمشتركين الذين انتهت صلاحيتهم.';
         } else {
             titleEl.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-orange-500" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>المشتركين على وشك الانتهاء';
-            descEl.textContent = 'قائمة مخصصة للمشتركين الذين تنتهي صلاحيتهم خلال 7 أيام.';
+            descEl.textContent = 'قائمة مخصصة للمشتركين الذين تنتهي صلاحيتهم خلال 3 أيام.';
         }
     }
     
